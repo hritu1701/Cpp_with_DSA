ea7b2d84bb8c3f972cbb7d8a4de5dc8521024a47
HRITU RAJ <112776533+hritu1701@users.noreply.github.com>
2023-09-09 16:00:10 +0530

    Added input code

---
+++ input.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int apples;
+    cout<<"Enter the no of Apples: ";
+    cin>>apples;
+
+    cout<<"No of apples is : "<<apples;
+    cout<<"Thankyou";
+    return 0;
+}
