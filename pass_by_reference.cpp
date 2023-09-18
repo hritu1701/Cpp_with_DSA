66b4c9d92e02875cceb9b87c1ab6bb8bb0272278
HRITU RAJ <112776533+hritu1701@users.noreply.github.com>
2023-09-18 21:22:31 +0530

    updated call by reference

--- pass_by_reference.cpp
+++ pass_by_reference.cpp
@@ -1,12 +1,18 @@
 #include<iostream>
 using namespace std;
 
-int q=8;
+void changeValue(int &z,int &y)
+{
+    z=100;
+    y=99;
+}
+
 int main()
 {
   int p=10;
-  int &q=p;
-    
-    cout<<q<<endl;
+  int q=9;
+
+  changeValue(p,q);
+  cout<<p<<" "<<q;
    
 }
