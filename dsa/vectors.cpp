829e2f97009c3bfbc3aec944a1aed9ba4a3af2a6
HRITU RAJ <112776533+hritu1701@users.noreply.github.com>
2023-09-22 23:29:43 +0530

    updated vector

--- dsa/vectors.cpp
+++ dsa/vectors.cpp
@@ -6,5 +6,25 @@ int main()
 {
     vector<int>arr;
     cout<<"Size: "<<arr.size()<<endl;
-    cout<<"Capacity"<<arr.capacity();
+    cout<<"Capacity: "<<arr.capacity()<<endl;
+
+    arr.push_back(10);
+    cout<<"Size: "<<arr.size()<<endl;
+    cout<<"Capacity: "<<arr.capacity()<<endl;
+
+    arr.push_back(20);
+    cout<<"Size: "<<arr.size()<<endl;
+    cout<<"Capacity: "<<arr.capacity()<<endl;
+
+    arr.push_back(30);
+    cout<<"Size: "<<arr.size()<<endl;
+    cout<<"Capacity: "<<arr.capacity()<<endl;
+
+    arr.push_back(40);
+    cout<<"Size: "<<arr.size()<<endl;
+    cout<<"Capacity: "<<arr.capacity()<<endl;
+
+    arr.resize(5);
+    cout<<"Size: "<<arr.size()<<endl;
+    cout<<"Capacity: "<<arr.capacity()<<endl;
 }
