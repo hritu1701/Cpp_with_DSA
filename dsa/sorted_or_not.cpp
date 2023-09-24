3b8b55b384bcb17ae2d126c8f44aacd896b8ce26
HRITU RAJ <112776533+hritu1701@users.noreply.github.com>
2023-09-24 20:31:25 +0530

    updated sorted

--- dsa/sorted_or_not.cpp
+++ dsa/sorted_or_not.cpp
@@ -7,10 +7,17 @@ int main()
     int arr[]={1,2,3,4,5,6};
     int size=sizeof(arr)/sizeof(arr[0]);
 
-    for(int i=0;i<size;i++)
+
+bool sortedFlag=true;
+    for(int i=1;i<size;i++)
     {
-        
+        if(arr[i] <= arr[i-1])
+        {
+            sortedFlag=false;
+        }
     }
+
+    cout<<sortedFlag<<endl;
 
 
 }
