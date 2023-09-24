2dd5662705830fb92cbad2e9be72a9f488f9e6c7
HRITU RAJ <112776533+hritu1701@users.noreply.github.com>
2023-09-24 21:24:23 +0530

    solved target sum problem 1

---
+++ dsa/target_sum1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    int arr[]={1,2,3,4,5};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    int targetSum=7;
+    int answer=0;
+
+    for(int i=0;i<size;i++)
+    {
+        for(int j=i+1;j<size;j++)
+        {
+            if(arr[i]+arr[j]==7)
+            {
+                answer+=1;
+            }
+        }
+    }
+    cout<<answer<<endl;
+}
